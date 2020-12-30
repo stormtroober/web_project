cf0f48f8295ab85f34db5779e34de31a78876a10
aleshark87 <aleshark87>
2020-12-30 11:23:30 +0100

    changed id to i in ste query

--- php/db/database.php
+++ php/db/database.php
@@ -13,7 +13,7 @@ class DatabaseHelper{
 
     public function getArticleById($id){
         $stmt = $this->db->prepare("SELECT nome, tipo, marca, foto, descrizione, prezzo, quantità FROM prodotti WHERE id = ?");
-        $stmt->bind_param("id", $id);
+        $stmt->bind_param('i', $id);
         $stmt->execute();
         $result = $stmt->get_result();
 
@@ -22,13 +22,11 @@ class DatabaseHelper{
     
     public function getPostByCategory($idcategory){
         /*$query = "SELECT idarticolo, titoloarticolo, imgarticolo, anteprimaarticolo, dataarticolo, nome FROM articolo, autore, articolo_ha_categoria WHERE categoria=? AND autore=idautore AND idarticolo=articolo";*/
-        $query = "SELECT ID, Nome, Marca, Foto, Prezzo, Quantità FROM prodotti WHERE Tipo = ?";
+        $query = "SELECT ID, Nome, Marca, Foto, Prezzo, Quantità FROM PRODOTTI WHERE Tipo = ?";
         $stmt = $this->db->prepare($query);
-        print_r($this->db->error_list);
         $stmt->bind_param('i',$idcategory);
         $stmt->execute();
         $result = $stmt->get_result();
-
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
--- php/template/article_list.php
+++ php/template/article_list.php
@@ -1,25 +1,24 @@
 <?php foreach($templateParams["articoli"] as $articolo): ?>
-    
     <main>
         <div class = "col-12">
                 <div class ="row m-5" style = "border-radius: 30px; background-color: #e8e8e8">
                     <div class = "col-12 col-md-5 col-lg-4 col-xl-3 col-xxl-2 text-center">         
-                        <img src="<?php echo UPLOAD_DIR/img/$articolo["Foto"]; ?>" class = "img-fluid p-3 w-50" style = "border-radius: 30px" alt="porcodio">
+                        <img src="<?php echo ../UPLOAD_DIR/img/$articolo["Foto"]; ?>" class = "img-fluid p-3 w-50" style = "border-radius: 30px" alt="porcodio">
                     </div>
                     <div class = "col-12 col-md-5 pt-3">
                         <span class="fs-4 fs-md-3 fs-lg-2 fs-xl-1 px-2"><?php echo $articolo["Nome"]; ?></span>
                         <div class="my-3 my-lg-4">
                             <div class = "d-flex flex-column">
                                 <div class = "d-flex">
-                                    <img src="../resources/icons/tick.png" height="35px" width="25px" class="me-2" alt="tick verify">
+                                    <img src="../../resources/icons/tick.png" height="35px" width="25px" class="me-2" alt="tick verify">
                                     <span class = "fs-5 fs-xl-3">Design cazzuto</span>
                                 </div>
                                 <div class = "d-flex">
-                                    <img src="../resources/icons/tick.png" height="35px" width="25px" class = "me-2" alt="tick verify">
+                                    <img src="../../resources/icons/tick.png" height="35px" width="25px" class = "me-2" alt="tick verify">
                                     <span class = "text_list">Colore spaccoso</span>
                                 </div>
                                 <div class = "d-flex">
-                                    <img src="../resources/icons/tick.png" height="35px" width="25px" class = "me-2" alt="tick verify">
+                                    <img src="../../resources/icons/tick.png" height="35px" width="25px" class = "me-2" alt="tick verify">
                                     <span class = "text_list">Leva Tremolo</span>
                                 </div>
                             </div>  
