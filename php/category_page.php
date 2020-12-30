6c39bcf6ad918440a2cb301c17234a6bfb85b805
aleshark87 <aleshark87>
2020-12-30 10:55:27 +0100

    dynamic category pt1

--- php/category_page.php
+++ php/category_page.php
@@ -1,129 +1,13 @@
-<!doctype html>
-<html lang="it">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <title>Guitar Benter</title>
+<?php
+require_once("db/include.php");
 
-    <!-- jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+$templateParams["nome"] = "article_list.php";
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+$idcategoria = -1;
+if(isset($_GET["type"])){
+    $idcategoria = $_GET["type"];
+}
+$templateParams["articoli"] = $dbh->getPostByCategory($idcategoria);
 
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="../style/style.css">
-    
-    <!-- JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-    <title>Category Page</title>
-  </head>
-    <body class="bg-light">
-      <div class="container-fluid">
-          <?php require("header.php") ?>
-        <div class = "col-12">
-            <div class ="row m-5" style = "border-radius: 30px; background-color: #e8e8e8">
-                <div class = "col-12 col-md-5 col-lg-4 col-xl-3 col-xxl-2 text-center">         
-                    <img src="../resources/img/PRS/front.jpg" class = "img-fluid p-3 w-50" style = "border-radius: 30px" alt="PRS guitar">
-                </div>
-                <div class = "col-12 col-md-5 pt-3">
-                    <span class="fs-4 fs-md-3 fs-lg-2 fs-xl-1 px-2">Title of the Article</span>
-                    <div class="my-3 my-lg-4">
-                        <div class = "d-flex flex-column">
-                            <div class = "d-flex">
-                                <img src="../resources/icons/tick.png" height="35px" width="25px" class="me-2" alt="tick verify">
-                                <span class = "fs-5 fs-xl-3">Design cazzuto</span>
-                            </div>
-                            <div class = "d-flex">
-                                <img src="../resources/icons/tick.png" height="35px" width="25px" class = "me-2" alt="tick verify">
-                                <span class = "text_list">Colore spaccoso</span>
-                            </div>
-                            <div class = "d-flex">
-                                <img src="../resources/icons/tick.png" height="35px" width="25px" class = "me-2" alt="tick verify">
-                                <span class = "text_list">Leva Tremolo</span>
-                            </div>
-                        </div>  
-                    </div>  
-                </div>
-                <div class = "col-md-2 col-lg-3 col-xl-4 col-xxl-5 text-center px-4 py-3">
-                    <div class="mt-md-3 mt-lg-5">
-                        <span class="fs-4 fs-xl-3"> Price </span>
-                    </div>
-                    <span class="fs-3 fs-xl-2">99.90$</span>
-                </div>
-                <div class = "col-md-1"></div>
-            </div>
-        </div>
-        <div class = "col-12">
-            <div class ="row m-5" style = "border-radius: 30px; background-color: #e8e8e8">
-                <div class = "col-12 col-md-5 col-lg-4 col-xl-3 col-xxl-2 text-center">         
-                    <img src="../resources/img/Les Paul/front.jpg" class = "img-fluid p-3 w-50" style = "border-radius: 30px" alt="PRS guitar">
-                </div>
-                <div class = "col-12 col-md-5 pt-3">
-                    <span class="fs-4 fs-md-3 fs-lg-2 fs-xl-1 px-2">Title of the Article</span>
-                    <div class="my-3 my-lg-4">
-                        <div class = "d-flex flex-column">
-                            <div class = "d-flex">
-                                <img src="../resources/icons/tick.png" height="35px" width="25px" class="me-2" alt="tick verify">
-                                <span class = "fs-5 fs-xl-3">Design cazzuto</span>
-                            </div>
-                            <div class = "d-flex">
-                                <img src="../resources/icons/tick.png" height="35px" width="25px" class = "me-2" alt="tick verify">
-                                <span class = "text_list">Colore spaccoso</span>
-                            </div>
-                            <div class = "d-flex">
-                                <img src="../resources/icons/tick.png" height="35px" width="25px" class = "me-2" alt="tick verify">
-                                <span class = "text_list">Leva Tremolo</span>
-                            </div>
-                        </div>  
-                    </div>  
-                </div>
-                <div class = "col-md-2 col-lg-3 col-xl-4 col-xxl-5 text-center px-4 py-3">
-                    <div class="mt-md-3 mt-lg-5">
-                        <span class="fs-4 fs-xl-3"> Price </span>
-                    </div>
-                    <span class="fs-3 fs-xl-2">99.90$</span>
-                </div>
-                <div class = "col-md-1"></div>
-            </div>
-        </div>
-        <div class = "col-12">
-            <div class ="row m-5" style = "border-radius: 30px; background-color: #e8e8e8">
-                <div class = "col-12 col-md-5 col-lg-4 col-xl-3 col-xxl-2 text-center">         
-                    <img src="../resources/img/Les Paul/front.jpg" class = "img-fluid p-3 w-50" style = "border-radius: 30px" alt="PRS guitar">
-                </div>
-                <div class = "col-12 col-md-5 pt-3">
-                    <span class="fs-4 fs-md-3 fs-lg-2 fs-xl-1 px-2">Title of the Article</span>
-                    <div class="my-3 my-lg-4">
-                        <div class = "d-flex flex-column">
-                            <div class = "d-flex">
-                                <img src="../resources/icons/tick.png" height="35px" width="25px" class="me-2" alt="tick verify">
-                                <span class = "fs-5 fs-xl-3">Design cazzuto</span>
-                            </div>
-                            <div class = "d-flex">
-                                <img src="../resources/icons/tick.png" height="35px" width="25px" class = "me-2" alt="tick verify">
-                                <span class = "text_list">Colore spaccoso</span>
-                            </div>
-                            <div class = "d-flex">
-                                <img src="../resources/icons/tick.png" height="35px" width="25px" class = "me-2" alt="tick verify">
-                                <span class = "text_list">Leva Tremolo</span>
-                            </div>
-                        </div>  
-                    </div>  
-                </div>
-                <div class = "col-md-2 col-lg-3 col-xl-4 col-xxl-5 text-center px-4 py-3">
-                    <div class="mt-md-3 mt-lg-5">
-                        <span class="fs-4 fs-xl-3"> Price </span>
-                    </div>
-                    <span class="fs-3 fs-xl-2">99.90$</span>
-                </div>
-                <div class = "col-md-1"></div>
-            </div>
-        </div>
-        <?php require("footer.php") ?>
-    </div>
-    </body>
-  
-</html>
+require("template/base.php");
+?>
