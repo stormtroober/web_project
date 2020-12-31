0053126bf7dbc56a467b08e59bb956099ec263f1
Stefano <stedamb@gmail.com>
2020-12-31 11:28:43 +0100

    added type link to article page

--- php/article_page.php
+++ php/article_page.php
@@ -3,10 +3,15 @@ require_once("db/include.php");
 
 $templateParams["nome"] = "article_page_template.php";
 
-$idArticolo = 1;
+$articleID = 1;
 if(isset($_GET["id"])){
-    $idArticolo = $_GET["id"];
+    $articleID = $_GET["id"];
 }
-$templateParams["articolo"] = $dbh->getArticleById($idArticolo);
+$articleType = "Guitar";
+if(isset($_GET["type"])){
+    $articleType = $_GET["type"];
+}
+$templateParams["articolo"] = $dbh->getArticleById($articleID);
+
 require("template/base.php");
 ?>
