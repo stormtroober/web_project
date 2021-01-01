cee449cf3e5d22ac4ba596d2db1fab3046c78afb
Stefano <stedamb@gmail.com>
2021-01-01 16:33:38 +0100

    updated article list

--- php/template/article_list.php
+++ php/template/article_list.php
@@ -5,7 +5,7 @@
                     <div class = "col-12 col-md-5 col-lg-4 col-xl-3 col-xxl-2 text-center">       
                         <img src="<?php echo UPLOAD_DIR."img/".$templateParams["categoria"]."/".$articolo["Foto"]."/front.png"; ?>" class = "img-fluid p-3 w-50" style = "border-radius: 30px" alt="porcodio">
                     </div>
-                    <div class = "col-12 col-md-5 pt-3">
+                    <div class = "col-12 col-md-7 col-lg-5 pt-3">
                         <a  class = "text-decoration-none text-dark h3" 
                             href="article_page.php?id=<?php echo $articolo["ID"]."&type=".$templateParams["categoria"]; ?>" class="fs-4 fs-md-3 fs-lg-2 fs-xl-1 px-2">
                             <?php echo $articolo["Nome"]; ?>
@@ -30,11 +30,11 @@
                             </div>  
                         </div>  
                     </div>
-                    <div class = "col-md-2 col-lg-3 col-xl-4 col-xxl-5 text-center px-4 py-3">
+                    <div class = "col-md-12 col-lg-3 col-xl-4 col-xxl-5 text-center px-4 py-3">
                         <div class="mt-md-3 mt-lg-5">
                             <span class="fs-4 fs-xl-3"> Price </span>
                         </div>
-                        <span class="fs-3 fs-xl-2"><?php echo $articolo["Prezzo"]." $"; ?></span>
+                        <span class="fs-3 fs-xl-2"><?php echo $articolo["Prezzo"]."$"; ?></span>
                     </div>
                     <div class = "col-md-1"></div>
                 </div>
