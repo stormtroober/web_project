14f1892a5608acc9979d026dc468db732077aa0c
aleshark87 <aleshark87>
2021-01-02 11:26:26 +0100

    now available works

--- JS/available.js
+++ JS/available.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-    let text = this.querySelectorAll('#available').text();
-    if(parseInt(text) == 0){
-        $("#available").text("Disponibile");
-        $("#available").css("color", "green");  
-    }
-    else{
-        $("#available").text("Non Disponibile");
-        $("#available").css("color", "red");
-    }
+    let spans = jQuery("[id=available]");
+    spans.each(function() {
+        if(parseInt($(this).text()) > 0){
+            $(this).text("Disponibile");
+            $(this).css("color", "green");  
+        }
+        else{
+            $(this).text("Non Disponibile");
+            $(this).css("color", "red");
+        }
+    })
 });
