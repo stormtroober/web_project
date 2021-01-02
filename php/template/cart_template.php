796ed42b3f396b33f6778c3615b42be36912e4e3
Stefano <stedamb@gmail.com>
2021-01-02 15:10:26 +0100

    finished cart

--- php/template/cart_template.php
+++ php/template/cart_template.php
@@ -5,24 +5,33 @@
       <div class="list-group px-5 px-md-0">
         <a href="user_page.html" class="list-group-item list-group-item-action bg-dark text-white">Prodotti</a>
         <div class="list-group-item">
-          <img src="../resources/img/Guitar/Fender_61_Telecaster/front.png" class="float-start m-2" width="85px">
-          <a href="" class="fs-3 text-decoration-none px-4">Nome Articolo</a>
-          <span class="float-end position-absolute bottom-0 end-0 p-4">Quantità: 1</a>
+          <img src="../resources/img/Guitar/Fender_61_Telecaster/front.png" class="float-start m-2" width="75px">
+          <a href="" class="fs-4 text-dark px-4">Nome Articolo</a>
+          <p class="fs-3 text-decoration-none p-4 float-end">Prezzo</a>
+          <p class="float-end position-absolute bottom-0 end-0 p-4">Quantità: 1</a>
         </div>
         <div class="list-group-item">
-          <img src="../resources/img/Guitar/Fender_61_Telecaster/front.png" class="float-start" width="85px">
-          <a href="" class="float-none fs-3 text-decoration-none p-4 mt-4">Nome Articolo</a>
-          <span class="float-end position-absolute bottom-0 end-0 p-4">Quantità: 1</a>
+          <img src="../resources/img/Guitar/Fender_61_Telecaster/front.png" class="float-start m-2" width="75px">
+          <a href="" class="fs-4 text-dark px-4">Nome Articolo</a>
+          <p class="fs-3 text-decoration-none p-4 float-end">Prezzo</a>
+          <p class="float-end position-absolute bottom-0 end-0 p-4">Quantità: 1</a>
         </div>
       </div>
     </div>
     <div class="col-md-1"></div>
     <div class="col-12 col-md-5 p-5 py-md-1">
       <h3>Carrello</h3>
-      <div class="list-group-item">
-        <a href="" class="list-group-item list-group-item-action bg-dark text-white">Prodotti</a>
-
+      <ul class="list-group pt-3">
+        <li class="list-group-item">Articolo 1<span class="float-end">99.90$</span></li>
+        <li class="list-group-item">Articolo 2<span class="float-end">99.90$</span></li>
+        <li class="list-group-item">Spedizione<span class="float-end">99.90$</span></li>
+        <li class="list-group-item bg-dark text-white">Totale<span class="float-end">99.90$</span></li>
+      </ul>
+      <div class="container-fluid">
+        <button type="button" class="btn btn-dark fs-4 float-end mt-4">Buy now</button>
       </div>
+    </div>
+
     <div class="col-md-1"></div>
   </div>
 </main>
