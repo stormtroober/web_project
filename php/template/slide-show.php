a95846d312dcd0e6203fa1ecfd08bd723d3bb837
Stefano <stedamb@gmail.com>
2021-01-06 16:41:58 +0100

    Update slide-show.php

--- php/template/slide-show.php
+++ php/template/slide-show.php
@@ -47,7 +47,7 @@
     </div>
     <div class="row">
         <div class="col-md-2"></div>
-        <div class="col-md-8 col-12 text-center">
+        <div class="col-md-8 col-12 text-center py-3">
             <h3>Welcome to Guitar Benter Online Store!</h3>
             <p>In this shop you will find all the instruments that you are searching, guitars, 
             guitar basses, amplifiers and accessories. We have the best brands, Super low prices and much more!</p>
