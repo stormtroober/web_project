9eb1cac5ae6f8618cafbcb085913eb7410b98077
Stefano <stedamb@gmail.com>
2021-01-06 12:16:19 +0100

    added notifications.php

---
+++ php/notifications.php
@@ -0,0 +1,19 @@
+<?php
+require_once("db/include.php");
+require_once("utils/functions.php");
+
+$templateParams["nnotifiche"] = $dbh->getNotificationsNumber();
+
+sec_session_start();
+if(login_check($dbh->getDb()) == true){
+  $templateParams["nome"] = "notifications_template.php";
+  $userEmail = $_SESSION['user_id'];
+
+}
+else{
+  $templateParams["nome"] = "slide-show.php";  
+}
+
+
+require("template/base.php");
+?>
---
+++ php/template/notifications_template.php
@@ -0,0 +1,16 @@
+<div class="row">
+  <div class="col-lg-2"></div>
+    <div class="col-12 col-lg-8 mt-5">
+    <h2 class="p-3">Notifications</h2>
+      <!-- foreach -->
+      <ul class="list-group">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Morbi leo risus</li>
+        <li class="list-group-item">Porta ac consectetur ac</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+      <button class="btn btn-dark float-end m-3">Clear</button>
+    </div>
+  <div class="col-lg-2"></div>
+</div>
