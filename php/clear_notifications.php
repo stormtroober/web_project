d23616c44da8fd2e7aa47b93ea90ae6f4d0f1004
Thomas Castellani <thomas.castellani@studio.unibo.it>
2021-01-06 18:04:57 +0100

    restored notifications

--- php/clear_notifications.php
+++ php/clear_notifications.php
@@ -2,21 +2,15 @@
 require_once("db/include.php");
 require_once("utils/functions.php");
 
-$url = "index.php";
-if(isset($_GET["url"])) {
-    $url = $_GET["url"];
-    echo $url;
-}
-
 sec_session_start();
 if(login_check($dbh->getDb()) == true){
     $dbh->deleteNotifications();
     $templateParams["nnotifiche"] = $dbh->getNotificationsNumber();
-    header("Location: ".$url);
+    $templateParams["nome"] = "slide-show.php";
 }
 else{
     $templateParams["nnotifiche"] = $dbh->getNotificationsNumber();
-    header("Location: ".$url);
+    $templateParams["nome"] = "slide-show.php";
 }
 
 require("template/base.php");
--- php/template/header.php
+++ php/template/header.php
@@ -43,7 +43,7 @@
               endforeach;
             ?>
             <li class="dropdown-item text-center">
-              <a class="btn text-decoration-none text-primary w-100" href="./clear_notifications.php?url=<?php echo basename($_SERVER['REQUEST_URI']); ?>">Clear All</a>
+              <a class="btn text-decoration-none text-primary w-100" href="./clear_notifications.php">Clear All</a>
             </li>
             </ul>
           </li>
